af24369b06d4601496a99a2bb9f43025e620b473
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2018-10-07 18:18:45 +0200

    Testing the loading of colly added Recipe Struct

---
+++ pkg/entity/recipe.go
@@ -0,0 +1,32 @@
+package entity
+
+// TODO add reviews as well
+type Recipe struct {
+	name string
+	originalUrl string
+	prepTimeRaw string
+	cookTimeRaw string
+	totalTimeRaw string
+	imageUrls []string
+	categories []string
+	description string
+	ingredients []string
+	instructions string
+	yield string
+
+	author struct {
+		typeAuthor string
+		name string
+	}
+
+	nutrition struct {
+		typeNutrition string
+		calories string
+		fatContent string
+		cholesterolContent string
+		sodiumContent string
+		carbohydrateContent string
+		fiberContent string
+		proteinContent string
+	}
+}
